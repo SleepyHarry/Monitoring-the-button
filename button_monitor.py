c61b85beb792108d79e1932dad4af16392b4ac94
Harry Vane <harryavane@gmail.com>
2015-05-10 01:08:23 +0100

    fixed not retrieving dynamic websocket URL
    - made `ButtonMonitor.ws_url` a property too because they're cool and it
    won't break anything that works off this
    - note: this will always get the correct websocket URL, since the real
    one appears near the top of the source (`response.text`), so even if
    someone posts with a title matching our regex, we won't be affected.

--- button_monitor.py
+++ button_monitor.py
@@ -3,19 +3,15 @@ import os
 import threading
 import time
 import json
+import re
+import requests
+
 from pprint import pprint
 
-class ButtonMonitor():
-    #annoyingly, this is dynamic
-    #I don't use this enough to bother getting it dynamically
-    #it can be done just by requesting /r/thebutton and searching
-    #(using regex) for a wss url
-
-    #TODO: try/except when this happens, so that the user understands
-    #      why something went wrong
-    ws_url = "wss://wss.redditmedia.com/thebutton?h=\
-9b98bf9ebcfb2c56f9128208991995f441371868&e=1429530922"
-
+class ButtonMonitor:
+    _ws_url_re = re.compile(r"wss://wss.redditmedia.com/thebutton\?h=[0-9a-f]+"
+                           r"&e=[0-9]+")
+    
     def __init__(self, callback, log_callback=None):
         #callback should be a callable object
         #the idea here is that whatever messages are recieved will be passed
@@ -23,19 +19,30 @@ class ButtonMonitor():
 
         #for example, if callback==print (as a function), then whenever
         #a message is recieved, it'll be printed (in unformatted JSON)
-
-        def black_hole(*args):
-            pass
         
         self.callback = callback
 
         #log_callback has a similar signature to callback, but it's called
         #when opens, closes and errors occur
-        self.log_callback = log_callback or black_hole
-
+        self.log_callback = log_callback or (lambda *a: None)
+        
         self.ws = None
         self.old_ws = None
         self.t = None
+
+    @property
+    def ws_url(self):
+        response = requests.get("http://www.reddit.com/r/thebutton")
+
+        if not response.ok:
+            raise requests.exceptions.ConnectionError("Cannot connect to reddit")
+
+        match = self._ws_url_re.search(response.text)
+
+        if match:
+            return match.group()
+
+        raise Exception("Cannot find a websocket address")
 
     def active_ws(self):
         return self.ws is not None
@@ -56,7 +63,7 @@ class ButtonMonitor():
             self.log_callback("websocket {} opened".format(ws.url))
         
 ##        websocket.enableTrace(True)
-        self.ws = websocket.WebSocketApp(ButtonMonitor.ws_url,
+        self.ws = websocket.WebSocketApp(self.ws_url,
                                   on_message = on_message,
                                   on_error = on_error,
                                   on_close = on_close,
